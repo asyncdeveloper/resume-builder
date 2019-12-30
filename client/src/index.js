2d9bb9b44853c60b72199db5d053ae476294fb2f
Oluwaseyi Adeogun <sheyilaaw98@gmail.com>
2019-12-30 12:27:27 +0100

    Setup folder structure for client server

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import React from "react";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
